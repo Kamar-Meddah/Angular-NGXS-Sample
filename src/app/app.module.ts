e1c4f0a88c0e77aad9718ebf21b8ad1c565d8c92
kamar Meddah <kamar.meddah@gmail.com>
2018-04-09 22:04:26 +0100

    chore(setup): Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {AppRoutingModule} from './app-routing.module';
+
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {AppComponent} from './app.component';
+
+import {environment} from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
